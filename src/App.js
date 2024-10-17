e48dec81fece285787ad73c9a5055c3b9886cea2
Usman Shaukat Gill <usman.gill@tuni.fi>
2024-10-18 00:42:40 +0300

    sample socket frontend

--- src/App.js
+++ src/App.js
@@ -1,10 +1,47 @@
+import React, { useEffect, useState } from 'react';
+import { io } from 'socket.io-client';
 import './App.css';
-import Home from './pages/home';
+
+const socket = io('http://localhost:3001');
 
 function App() {
+  const [isConnected, setIsConnected] = useState(false);
+  const [lastMessage, setLastMessage] = useState(null);
+
+  useEffect(() => {
+    socket.on('connect', () => {
+      setIsConnected(true);
+      console.log('Connected to server');
+    });
+
+    socket.on('disconnect', () => {
+      setIsConnected(false);
+      console.log('Disconnected from server');
+    });
+
+    socket.on('message', (message) => {
+      console.log('Received message:', message);
+      setLastMessage(message);
+    });
+
+    return () => {
+      socket.off('connect');
+      socket.off('disconnect');
+      socket.off('message');
+    };
+  }, []);
+
+  const sendMessage = () => {
+    socket.emit('message', 'Hello from client!');
+  };
+
   return (
-    <div>
-      <Home/>
+    <div className="App">
+      <header className="App-header">
+        <p>Connected: {'' + isConnected}</p>
+        <p>Last message: {lastMessage || '-'}</p>
+        <button onClick={sendMessage}>Send Message</button>
+      </header>
     </div>
   );
 }
